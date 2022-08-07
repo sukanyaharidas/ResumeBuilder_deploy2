c824efb697efd07a49146dd8c56503cd4a124cfd
sukanyaharidas <sukanyaharidas97@gmail.com>
2022-08-07 16:40:20 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -23,7 +23,7 @@ const dotenv = require("dotenv");
 const app = new express();
 const port = 4000;
 
-dotenv.config();
+
 app.use(cors());
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000}));
